b26890e4c39910c45a60dfa20a96f0eed54248a7
munimul hasan <munimul999@gmail.com>
2023-10-18 01:26:04 -0500

    added some style, stars api working

--- script.js
+++ script.js
@@ -2,15 +2,64 @@
 
 import data from "./sample.json" assert { type: "json" };
 
+// Single constellation paragraph, more details, and picture button
+const single = document.getElementById("single");
+const paragraph0 = document.getElementById("zero");
 const paragraph1 = document.getElementById("one");
 const paragraph2 = document.getElementById("two");
+const details = document.getElementById("more");
+const name = document.getElementById("name");
+const engName = document.getElementById("engName");
+const brightestStar = document.getElementById("brightestStar");
+const allStars = document.getElementById("allStars");
 
+// Hide details tag first
+single.classList.add("hidden");
+
+// Api Ninja
+let options = {
+  method: "GET",
+  headers: { "x-api-key": "HKIbrbUbfb/f8ocdY9jEig==XGtv6B3zpeKj1u9F" },
+};
+
+let url;
+
+// Api call function
+
+function apiCall(constel) {
+  url = `https://api.api-ninjas.com/v1/stars?constellation=${constel}`;
+  fetch(url, options)
+    .then((res) => res.json()) // parse response as JSON
+    .then((data) => {
+      if (data.length === 0) {
+        console.log("No response");
+        allStars.classList.add("hidden");
+      } else {
+        allStars.classList.remove("hidden");
+        allStars.textContent =
+          "Brightest star's distance from earth is : " +
+          data[0].distance_light_year +
+          " Light years." +
+          " It's spectral class is: " +
+          data[0].spectral_class;
+      }
+      console.log(data);
+    })
+    .catch((err) => {
+      console.log(`error ${err}`);
+    });
+}
+
+// create an array from data with constellations name
 let constellations = [];
-
 for (let i = 0; i < data.length; i++) {
   constellations.push(data[i].name);
 }
+
+// Constellations name list element
 const parentDiv = document.getElementById("allConste");
+
+// Create 88 buttons for constellations under parentDiv
 for (let i = 0; i < constellations.length; i++) {
   let child = document.createElement("button");
   child.textContent = constellations[i];
@@ -18,11 +67,24 @@ for (let i = 0; i < constellations.length; i++) {
   child.classList = "constEl";
   parentDiv.appendChild(child);
 }
+
+// Select all buttons
 const button = document.querySelectorAll(".constEl");
 
+// add event for all buttons
 for (let i = 0; i < constellations.length; i++) {
   button[i].addEventListener("click", function () {
+    name.textContent = data[i].name;
+    engName.textContent = data[i].englishName;
+    brightestStar.textContent = "Brightest Star: " + data[i].brightestStar;
+    paragraph0.textContent = data[i].paragraph0;
     paragraph1.textContent = data[i].paragraph1;
     paragraph2.textContent = data[i].paragraph2;
+    details.href = data[i].url;
+
+    // get information of first star from api call
+    apiCall(data[i].name);
+
+    single.classList.remove("hidden");
   });
 }
--- workProgress.md
+++ workProgress.md
@@ -11,3 +11,13 @@ Wikipedia has lots of information regarding constellations, their properties, na
 - BeautifulSoup was used to scrap the information. Prior to scrapping our dataset was processed accordingly, so that http request does not end up in a 404. A simple function was developed to find ' ' (spaces) between names and change it to an _ (underscore) for double word constellations and simply add an _ at the end of a single named constellation.
 
 - Only the first two paragraphs from each constellation were saved with the constellation name, the brightest star and other information and this attributes were transformed into a dictonary which later appended to a list and thus creating the sample.json file.
+
+## Showing picture from an API
+
+Showing a constellation's pic from an api call was tried but failed due to not appropriate tagging of api images or muliple meaning and various usage of constellation names in multiple situation/objects.
+
+- We will once again resort to scrapping images from wikipedia as it has more flexibility for using its open-source images
+
+## More details about the brightest star for each constellation
+
+- An api from api ninja called 'Stars' was used to display more information for each constellations brightest stars.
